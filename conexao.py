d54cb4d9f010509b99e2d5f50c6d81b42936d0a0
Ana Carolina <aninhasantos657432@gmail.com>
2026-04-23 11:44:19 -0300

    Update database server connection string

--- conexao.py
+++ conexao.py
@@ -2,7 +2,7 @@ import pyodbc
 
 conexao = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=TBS0676758W11-1\\SQLEXPRESS;'
+    'SERVER=seuserver;'
     'DATABASE=Biblioteca_Renovada;'
     'Trusted_Connection=yes;'
 )
